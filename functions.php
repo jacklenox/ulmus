e06e1612182d7f873efabc001aae94fe3ea9065f
Jack Lenox <jacklenox@fastmail.com>
2017-01-25 13:28:40 -0500

    Remove unnecessary debugging code

--- functions.php
+++ functions.php
@@ -55,27 +55,3 @@ function ulmus_scripts() {
 	wp_enqueue_script( 'ulmus-theme', get_template_directory_uri() . '/ulmus.js', array(), '20170118', true );
 }
 add_action( 'wp_enqueue_scripts', 'ulmus_scripts' );
-
-function get_post_data( $posts = null ) {
-	//if ( $posts === null && ! is_404() ) {
-		$posts = $GLOBALS['wp_query']->posts;
-	//}
-	//error_log( print_r( $GLOBALS['wp_query']->posts, true ) );
-	global $wp_rest_server;
-	if ( empty( $wp_rest_server ) ) {
-		$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
-		$wp_rest_server       = new $wp_rest_server_class;
-		do_action( 'rest_api_init' );
-	}
-	$data               = array();
-	$request            = new \WP_REST_Request();
-	$request['context'] = 'view';
-	foreach ( (array) $posts as $post ) {
-		$controller = new \WP_REST_Posts_Controller( $post->post_type );
-		$data[]     = $wp_rest_server->response_to_data( $controller->prepare_item_for_response( $post, $request ), true );
-	}
-	error_log( print_r( $data, true ) );
-	return $data;
-}
-
-add_action( 'wp_footer', 'get_post_data' );
